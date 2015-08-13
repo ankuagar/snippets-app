545222a9df0c64ab74c2182bd42f9fce9f54ed2e
Ankur Agarwal <ankur.a.agarwal@gmail.com>
2015-08-13 05:26:34 +0000

    Add parser structure

--- snippets.py
+++ snippets.py
@@ -1,29 +1,61 @@
 #!/usr/bin/env python
 
+import argparse
 import logging
+import sys
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
-def put(name, snippet):
-    """
-    Store a snippet with an associated name.
+def main():
+	"""Main function"""
+	logging.info("Constructing parser")
+	parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
 
-    Returns the name and the snippet
-    """
-    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
-    print "hello"	
-    return name, snippet
+	subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+	# Subparser for the put command
+	logging.debug("Constructing put subparser")
+	put_parser = subparsers.add_parser("put", help="Store a snippet")
+	put_parser.add_argument("name", help="The name of the snippet")
+	put_parser.add_argument("snippet", help="The snippet text")
+	# Subparser for the get command 
+	logging.debug("Constructing get subparser")
+	get_parser = subparsers.add_parser("get", help="Get a snippet")
+	get_parser.add_argument("name", help="The name of the snippet")
+
+	arguments = parser.parse_args(sys.argv[1:])
+
+	# Convert parsed arguments from Namespace to dictionary
+	arguments = vars(arguments)
+	command = arguments.pop("command")
+
+	if command == "put":
+        	name, snippet = put(**arguments)
+	        print("Stored {!r} as {!r}".format(snippet, name))
+	elif command == "get":
+        	snippet = get(**arguments)
+	        print("Retrieved snippet: {!r}".format(snippet))
+
+def put(name, snippet):
+	"""
+	Store a snippet with an associated name.
+
+	Returns the name and the snippet
+	"""
+	logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+	return name, snippet
 
 def get(name):
-    """Retrieve the snippet with a given name.
+	"""Retrieve the snippet with a given name.
 
-    If there is no such snippet...
-
-    Returns the snippet.
-    """
-    logging.error("FIXME: Unimplemented - get({!r})".format(name))
-    return ""
+	If there is no such snippet return None"""
 
 
-put(None, None)
+	logging.error("FIXME: Unimplemented - get({!r})".format(name))
+	return ""
+
+
+
+if __name__ == '__main__':
+	main()
